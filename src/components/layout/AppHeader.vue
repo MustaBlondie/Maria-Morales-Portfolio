1d301eae229eec43b6993ffb4a1efcf126c7e7a8
Mav MoZu <victorina23x@gmail.com>
2026-02-24 12:31:35 -0400

    style: adjust hero section layout for proper responsive spacing

--- src/components/layout/AppHeader.vue
+++ src/components/layout/AppHeader.vue
@@ -1,7 +1,7 @@
 <script setup></script>
 
 <template>
-  <header class="w-full sm:px-14 flex flex-col bg-[#FFF7F4]">
+  <header class="w-full sm:min-h-screen sm:px-14 flex flex-col bg-[#FFF7F4]">
     <div
       class="hidden sm:flex justify-between margin my-8 px-9 py-3 border-t border-b border-b-black font-semibold"
     >
@@ -10,12 +10,14 @@
       <div class="cursor-pointer">CONTACT</div>
       <div class="cursor-pointer">EN/SP</div>
     </div>
-    <div class="flex flex-col-reverse sm:flex-col sm:ml-auto sm:mt-96 mt-9 ml-5">
-      <h2 class="sm:text-8xl text-base sm:font-extrabold sm:p-3.5 -mt-2">MARIA MORALES</h2>
-      <h1 class="sm:text-4xl text-xl sm:font-semibold font-extrabold sm:text-right sm:px-3.5">
-        WEB DEVELOPER
-      </h1>
+
+    <div class="flex flex-1 sm:justify-end sm:items-end px-5 pt-3.5 sm:px-9 pb-10">
+      <div class="sm:text-right flex sm:flex-col flex-col-reverse mt-9">
+        <h2 class="sm:text-8xl text-base sm:font-extrabold sm:px-3.5 -mt-2">MARIA MORALES</h2>
+        <h1 class="sm:text-4xl text-xl sm:font-semibold font-extrabold sm:px-3.5">WEB DEVELOPER</h1>
+      </div>
     </div>
+
     <div class="ml-5 sm:mt-28 sm:mb-10 mb-2 mt-10">
       <button
         class="sm:text-lg text-xs font-semibold border-2 rounded sm:px-5 px-3.5 sm:py-0.5 py-1 cursor-pointer"
