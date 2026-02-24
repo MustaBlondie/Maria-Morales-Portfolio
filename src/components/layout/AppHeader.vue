a7eb7a146d5d837c179fde260a6fbf3cc6ec389c
Mav MoZu <victorina23x@gmail.com>
2026-02-24 12:46:44 -0400

    style: adjust mobile styles (button border radius and hero title margins)

--- src/components/layout/AppHeader.vue
+++ src/components/layout/AppHeader.vue
@@ -11,16 +11,18 @@
       <div class="cursor-pointer">EN/SP</div>
     </div>
 
-    <div class="flex flex-1 sm:justify-end sm:items-end px-5 pt-3.5 sm:px-9 pb-10">
+    <div class="flex flex-1 sm:justify-end sm:items-end px-5 pt-3.5 mb-4 sm:px-9">
       <div class="sm:text-right flex sm:flex-col flex-col-reverse mt-9">
-        <h2 class="sm:text-8xl text-base sm:font-extrabold sm:px-3.5 -mt-2">MARIA MORALES</h2>
+        <h2 class="sm:text-8xl text-base font-semibold sm:font-extrabold sm:px-3.5 -mt-2">
+          MARIA MORALES
+        </h2>
         <h1 class="sm:text-4xl text-xl sm:font-semibold font-extrabold sm:px-3.5">WEB DEVELOPER</h1>
       </div>
     </div>
 
     <div class="ml-5 sm:mt-28 sm:mb-10 mb-2 mt-10">
       <button
-        class="sm:text-lg text-xs font-semibold border-2 rounded sm:px-5 px-3.5 sm:py-0.5 py-1 cursor-pointer"
+        class="sm:text-lg text-xs font-semibold border-2 rounded-md sm:rounded-lg sm:px-5 px-3.5 sm:py-0.5 py-1 cursor-pointer"
       >
         RESUME
       </button>
