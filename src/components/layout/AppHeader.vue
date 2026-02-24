44a564b3896b6a6834f3140a172760d7aad7b69e
Mav MoZu <victorina23x@gmail.com>
2026-02-24 09:47:01 -0400

    adjust margins for better design

--- src/components/layout/AppHeader.vue
+++ src/components/layout/AppHeader.vue
@@ -16,7 +16,7 @@
         WEB DEVELOPER
       </h1>
     </div>
-    <div class="ml-5 sm:mt-28 sm:mb-10 mb-2 mt-8">
+    <div class="ml-5 sm:mt-28 sm:mb-10 mb-2 mt-10">
       <button
         class="sm:text-lg text-xs font-semibold border-2 rounded sm:px-5 px-3.5 sm:py-0.5 py-1 cursor-pointer"
       >
