5ff1313a3a8ee43f2c9db9ceb0706341d962c460
Mav MoZu <victorina23x@gmail.com>
2026-05-27 19:36:38 -0400

    style: add brand colors (#111314 and #FDD4BD) to Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#111314', // Color principal para bg en darkmode - letras en lightmode
+        secondary: '#FDD4BD', // Color principal para bg en lightmode - letras en darkmode
+      },
+    },
   },
   plugins: [],
 }
