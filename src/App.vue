e618e9834b82e0c367c2be858b41aa729270f6e1
Mav MoZu <victorina23x@gmail.com>
2026-05-27 17:11:18 -0400

    feat: add light bulb icon from game-icons using @iconify/vue

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,9 @@ import { Icon } from '@iconify/vue'
 </script>
 
 <template>
-  <div class="h-screen bg-[#111314]">
-    <div class="max-w-[1440px] mx-auto">
-      <div class="py-11 px-[570px]">
+  <div class="min-h-screen bg-[#111314]">
+    <div class="max-w-[1440px] mx-auto px-4 sm:px-6 lg:px-8">
+      <div class="py-11 px-4 sm:px-8 md:px-[570px]">
         <Icon icon="game-icons:light-bulb" class="text-[45px] text-[#F8B6BA] rotate-180" />
       </div>
     </div>
