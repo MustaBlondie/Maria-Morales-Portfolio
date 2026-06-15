4d8ca0537096871316c21b8229469b12241e8da6
Mav MoZu <victorina23x@gmail.com>
2026-06-15 17:25:49 -0400

    fix: Remove HomeView component from App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,7 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
-import HomeView from './views/HomeView.vue'
 </script>
 
 <template>
-  <HomeView />
   <RouterView />
 </template>
