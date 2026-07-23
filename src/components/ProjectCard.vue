40cfda43d2c49323e9cf69737323d66fc10e9e82
Mav MoZu <victorina23x@gmail.com>
2026-07-23 15:25:05 -0400

    feat: add ProjectSmallCard component template Create ProjectSmallCard component skeleton and add temporary texts in en.json for ProjectCard and ProjectSmallCard components

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -3,21 +3,29 @@
   <div class="flex mt-[100px]">
     <div class="grid grid-cols-2 grid-rows-2 gap-9">
       <div class="row-span-2 h-[682px]">
-        <img src="../assets/images/social.jpeg" alt="" class="w-full h-full object-cover" />
+        <img
+          src="../assets/images/social.jpeg"
+          alt="project_main_image"
+          class="w-full h-full object-cover"
+        />
       </div>
 
       <div class="w-[340px] h-[379px]">
-        <img src="../assets/images/1.jpeg" alt="" class="w-full h-full object-cover" />
+        <img
+          src="../assets/images/1.jpeg"
+          alt="project_second_image"
+          class="w-full h-full object-cover"
+        />
       </div>
 
       <div class="flex flex-col gap-y-3">
         <div>
-          <a href="#" class="font-bold text-[32px]">Social WEBSIDE</a>
-          <p class="font-semibold text-[32px]">2026</p>
+          <a href="#" class="font-bold text-[32px]">{{ $t('projects.projMainTitle') }}</a>
+          <p class="font-semibold text-[32px]">{{ $t('projects.projYear') }}</p>
         </div>
 
-        <p class="text-[32px]">Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
-        <p class="font-semibold text-[32px]">Front-End | TypeScript | Vue | Nuxt</p>
+        <p class="text-[32px]">{{ $t('projects.projIntro') }}</p>
+        <p class="font-semibold text-[32px]">{{ $t('projects.projTechStackList') }}</p>
       </div>
     </div>
   </div>
@@ -32,12 +40,12 @@
       </div>
       <div class="flex flex-col gap-y-3 text-right">
         <div>
-          <a href="#" class="font-bold text-[32px]">Social WEBSIDE</a>
-          <p class="font-semibold text-[32px]">2026</p>
+          <a href="#" class="font-bold text-[32px]">{{ $t('projects.projMainTitle') }}</a>
+          <p class="font-semibold text-[32px]">{{ $t('projects.projYear') }}</p>
         </div>
 
-        <p class="text-[32px]">Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
-        <p class="font-semibold text-[32px]">Front-End | TypeScript | Vue | Nuxt</p>
+        <p class="text-[32px]">{{ $t('projects.projIntro') }}</p>
+        <p class="font-semibold text-[32px]">{{ $t('projects.projTechStackList') }}</p>
       </div>
     </div>
   </div>
--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -1,12 +1,16 @@
 <script setup lang="ts">
 import Container from '@/components/Container.vue'
 import ProjectCard from '@/components/ProjectCard.vue'
+import ProjectSmallCard from '@/components/ProjectSmallCard.vue'
 </script>
 <template>
   <Container>
     <div id="about" class="mt-[200px] px-20">
       <h2 class="text-right font-bold text-[96px]">{{ $t('projects.projectsMainTitle') }}</h2>
       <ProjectCard />
+    </div>
+    <div>
+      <ProjectSmallCard />
     </div>
   </Container>
 </template>
