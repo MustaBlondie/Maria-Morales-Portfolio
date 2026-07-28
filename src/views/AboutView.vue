2cdfa4b3d995ddce4f150fd4b5a76d92c70d20d6
Mav MoZu <victorina23x@gmail.com>
2026-07-28 10:36:47 -0400

    style(template): adjust about section for mobile screens

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -7,32 +7,36 @@ const { borderClass } = useThemeBorder()
 
 <template>
   <Container>
-    <div id="about" class="mt-[200px] px-20">
-      <h4 class="font-bold text-[96px]">{{ $t('about.aboutTitle') }}</h4>
-      <div class="max-w-4xl mt-4">
-        <p class="text-[24px]">{{ $t('about.aboutP1') }}</p>
+    <div id="about" class="mt-[70px] md:mt-[200px] px-5 md:px-20">
+      <h4 class="font-bold text-[36px] md:text-[96px]">{{ $t('about.aboutTitle') }}</h4>
+      <div class="text-right tracking-[0.09em] md:text-left max-w-4xl mt-2 md:mt-4">
+        <p class="pl-10 md:pl-0 md:text-[24px]">{{ $t('about.aboutP1') }}</p>
 
-        <div>
-          <h4 class="text-[24px] font-extrabold pt-4 pb-2">{{ $t('about.aboutTitle2') }}</h4>
+        <div class="hidden md:block">
+          <h4 class="md:text-[24px] font-extrabold pt-4 pb-2">{{ $t('about.aboutTitle2') }}</h4>
           <div class="pl-5">
-            <ul class="list-disc pl-2 text-[20px]">
+            <ul class="list-disc pl-2 md:text-[20px]">
               <li v-for="(skill, index) in $tm('about.skillList')" :key="index">
                 {{ skill }}
               </li>
             </ul>
           </div>
         </div>
+        <div class="hidden md:block">
+          <h4 class="md:text-[24px] font-extrabold pt-4 pb-2">{{ $t('about.aboutTitle3') }}</h4>
+          <p class="md:text-[24px]">{{ $t('about.aboutP3') }}</p>
 
-        <h4 class="text-[24px] font-extrabold pt-4 pb-2">{{ $t('about.aboutTitle3') }}</h4>
-        <p class="text-[24px]">{{ $t('about.aboutP3') }}</p>
-
-        <h4 class="text-[24px] font-extrabold pt-4 pb-2">{{ $t('about.aboutTitle4') }}</h4>
-        <p class="text-[24px]">{{ $t('about.aboutP4') }}</p>
+          <h4 class="md:text-[24px] font-extrabold pt-4 pb-2">{{ $t('about.aboutTitle4') }}</h4>
+          <p class="md:text-[24px]">{{ $t('about.aboutP4') }}</p>
+        </div>
       </div>
-      <div class="mt-20">
+      <div class="mt-4 md:mt-20 text-right tracking-[0.09em] md:text-left">
         <h3 class="text-[20px] font-semibold">{{ $t('about.buttonHelperText1') }}</h3>
         <h4 class="text-[20px] font-semibold">{{ $t('about.buttonHelperText2') }}</h4>
-        <button class="border-4 mt-5 px-9 py-2 text-[24px] font-semibold" :class="borderClass">
+        <button
+          class="border-2 md:border-4 mt-3 md:mt-5 px-9 py-2 md:text-[24px] font-semibold"
+          :class="borderClass"
+        >
           {{ $t('nav.cv') }}
         </button>
       </div>
