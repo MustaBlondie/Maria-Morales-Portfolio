fe30fc533689ba02559b163eb0e6347d38533ab1
Mav MoZu <victorina23x@gmail.com>
2026-08-05 14:33:53 -0400

    style(ProjectSmallCard): adjust container gap for mobile screens

--- src/components/ProjectSmallCard.vue
+++ src/components/ProjectSmallCard.vue
@@ -2,7 +2,7 @@
 <template>
   <div class="flex flex-col mt-[100px] gap-8">
     <h2 class="font-semibold text-[36px]">{{ $t('otherPro.proMainTitle') }}</h2>
-    <div class="flex gap-24">
+    <div class="flex gap-7 md:gap-24">
       <div class="grid grid-rows-2 md:grid-cols-2 gap-4">
         <div class="md:w-[212px] md:h-[236px]">
           <img
